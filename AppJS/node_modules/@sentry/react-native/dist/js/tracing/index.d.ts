037f2756c967a6be222c669c5e475bbae4400bee
Thang Vo <thang.vohuynhduy@hungthinhcorp.com.vn>
2021-08-13 11:45:37 +0700

    implement skeleton

--- AppJS/node_modules/@sentry/react-native/dist/js/tracing/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { ReactNativeTracing } from "./reactnativetracing";
-export { RoutingInstrumentation, RoutingInstrumentationInstance, } from "./routingInstrumentation";
-export { ReactNavigationV5Instrumentation } from "./reactnavigationv5";
-export { ReactNavigationV4Instrumentation } from "./reactnavigationv4";
-export { ReactNavigationCurrentRoute, ReactNavigationRoute, ReactNavigationTransactionContext, } from "./types";
-//# sourceMappingURL=index.d.ts.map
